e346309669afe875b6207bccc1bfb911fccaaaad
Skyforce77 <arnaud.bruyer@gmail.com>
2015-03-19 06:40:10 +0100

    Moved to abstract

--- src/fr/skyforce77/arch3ds/api/Plugin.java
+++ src/fr/skyforce77/arch3ds/api/Plugin.java
@@ -2,54 +2,54 @@ package fr.skyforce77.arch3ds.api;
 
 import java.awt.Graphics2D;
 
-public interface Plugin {
+public abstract class Plugin {
 
 	/**
      * Returns the name of the plugin
      *
      * @return name of the plugin
      */
-	public String getName();
+	public abstract String getName();
 	
 	/**
      * Returns the version of the plugin
      *
      * @return version of the plugin
      */
-	public String getVersion();
+	public abstract String getVersion();
 	
 	/**
      * Called when this plugin need to be enabled
      */
-	public void onInit();
+	public abstract void onInit();
 	
 	/**
      * Called when this plugin is enabled
      */
-	public void onEnable();
+	public abstract void onEnable();
 	
 	/**
      * Called when this plugin is disabled
      */
-	public void onDisable();
+	public abstract void onDisable();
 	
 	/**
      * Called when server ticked
      */
-	public void onTick();
+	public abstract void onTick();
 	
 	/**
      * Called when server need to refresh topscreen
      * 
      * @return plugin topscreen graphics
      */
-	public Graphics2D drawTopScreen(Graphics2D graphics);
+	public abstract Graphics2D drawTopScreen(Graphics2D graphics);
 	
 	/**
      * Called when server need to refresh bottomscreen
      * 
      * @return plugin bottomscreen graphics
      */
-	public Graphics2D drawBottomScreen(Graphics2D graphics);
+	public abstract Graphics2D drawBottomScreen(Graphics2D graphics);
 	
 }
